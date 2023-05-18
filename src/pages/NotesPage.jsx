0713e15ab39736f9e2e0c98dd62a77d718736d27
Tevfik istanbullu <tevfikistanbullu94@gmail.com>
2023-05-18 11:44:33 +0300

    Update NotesPage.jsx

--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,7 +1,82 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-const NotesPage = () => {
-  return <div>NotesPage</div>;
-};
+function NotesPage() {
+  const [notes, setNotes] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch("https://ubade.pythonanywhere.com/api/notes", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setNotes(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleDelete = (id) => {
+    fetch("https://ubade.pythonanywhere.com/api/notes", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+      body: JSON.stringify({ id: id }),
+    })
+      .then((response) => response.json())
+      .then(() => {
+        // remove the deleted note from the state
+        setNotes(notes.filter((note) => note.id !== id));
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const handleLogout = async () => {
+    try {
+      const response = await fetch(
+        "https://ubade.pythonanywhere.com/api/logout",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      );
+      if (response.ok) {
+        localStorage.removeItem("token");
+        navigate("/");
+      } else {
+        throw new Error("Logout faild");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Notes Page</h1>
+      <Link to="/notes/add">
+        <button>Create New Note</button>
+      </Link>
+      <br />
+      <button onClick={handleLogout}>Logout</button>
+      {notes.map((note) => (
+        <div key={note.id}>
+          <h3>{note.title}</h3>
+          <p>{note.body}</p>
+
+          <button onClick={() => handleDelete(note.id)}>Delete</button>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default NotesPage;
